0aee3da55d37750b9e9dfd40341e4239fbbc37f5
Dosimjan <dk123tm@mail.ru>
2023-12-22 23:02:49 +0600

    cloned template repository

---
+++ src/models/Point.java
@@ -0,0 +1,26 @@
+package models;
+
+public class Point {
+    private double x;
+    private double y;
+
+
+
+
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+
+    toString
+    // constructor having both parameters
+
+    // distance(Point dest) - distance from current point to the dest point
+
+    // toString
+}
