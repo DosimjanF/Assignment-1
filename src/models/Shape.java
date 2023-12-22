39fdf38fecd87829fb9f1141e292be85a86a4737
dosimjan <95702474+DosimjanF@users.noreply.github.com>
2023-12-23 00:25:41 +0600

    Update Shape.java

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,6 +1,47 @@
 package models;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Shape {
+    private double perimeter = 0;
+
+    ArrayList points = new ArrayList<Point>;
+
+
+    double distances[] = new double[points.size()];
+
+
+    public addPoint(Point) {
+        points.add(Point);
+    }
+
+    public addLengths {
+        for (int i = 0; i < points.size() - 1; i++) {
+            distances[i] = points[i].distance(points[i + 1].getX, points[i + 1].getY);
+        }
+    }
+
+
+    public double calculatePerimeter() {
+
+        for (int i = 0; i < distances.size(); i++) {
+            perimeter += distances[i];
+        }
+        return perimeter;
+    }
+
+    public double getAvgSide() {
+        return perimeter / distances.size();
+    }
+
+    public double getLongestSide() {
+        double maxSide = distances[0];
+        for (int i = 1; i < distances.size(); i++) {
+            if (distances[i] > maxSide) maxSide = distances[i];
+        }
+        return maxSide;
+    }
 
     // container of Points (e.g. ArrayList<Point>)
 
